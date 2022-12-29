3856ad47c5f6d8a19647144cbf9bd83f632c53f0
siddharth <ranasiddharth6@gmail.com>
2022-12-29 23:01:57 +0530

    installing mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import theme from "./config/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
