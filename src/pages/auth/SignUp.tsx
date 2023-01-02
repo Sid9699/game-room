2285232d0478ed72c124cbd04b41fa7b4389ce1f
siddharth <ranasiddharth6@gmail.com>
2023-01-02 23:28:54 +0530

    login signup page ui

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,5 +1,51 @@
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+  Link,
+} from "@mui/material";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Box
+      sx={(theme) => ({
+        height: "100%",
+        background: `linear-gradient(to right, ${theme.palette.primary.main}, ${theme.palette.primary.dark})` /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */,
+      })}
+    >
+      <Container
+        maxWidth="xs"
+        sx={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Stack
+          bgcolor="whitesmoke"
+          spacing={4}
+          p={4}
+          borderRadius={2}
+          boxShadow={3}
+          sx={{ width: "100%" }}
+        >
+          <Typography variant="h4" textAlign="center">
+            Sign Up
+          </Typography>
+          <TextField label="Email" />
+          <TextField label="Password" type="password" />
+          <Button variant="contained">Sign Up</Button>
+          <Typography variant="body1" textAlign="center">
+            Already have an account? <Link href="/auth/login">Log In now</Link>
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
 
 export default SignUp;
